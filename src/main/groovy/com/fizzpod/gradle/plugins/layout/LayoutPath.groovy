6421b16da4248915cbea32c744d9c7db61201c57
Andrew Dunn <andy.dunn@expensys.com>
2015-03-10 21:03:56 +0000

    Added logging

--- src/main/groovy/com/fizzpod/gradle/plugins/layout/LayoutPath.groovy
+++ src/main/groovy/com/fizzpod/gradle/plugins/layout/LayoutPath.groovy
@@ -1,8 +1,11 @@
 package com.fizzpod.gradle.plugins.layout
 
-import java.io.File;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class LayoutPath {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LayoutPath)
 
 	def path
 	
@@ -11,9 +14,13 @@ class LayoutPath {
 	}
 	
 	public File write(def root) {
+		
 		def layoutDir = new File(root, path)
 		if(!layoutDir.exists()) {
+			LOGGER.info("Creating directory {}", layoutDir);
 			layoutDir.mkdirs()
+		} else {
+		LOGGER.info("Skipping directory {} as it already exists", layoutDir);
 		}
 		return layoutDir
 	}
